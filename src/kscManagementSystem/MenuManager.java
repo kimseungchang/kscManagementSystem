18f7da189a152555eeef7308d6bfd2c6f123de32
kimseungchang <ksc9716@gmail.com>
2021-04-03 13:26:15 +0900

    많이 고침

--- src/kscManagementSystem/MenuManager.java
+++ src/kscManagementSystem/MenuManager.java
@@ -5,65 +5,34 @@ import java.util.Scanner;
 public class MenuManager {
 
 	public static void main(String[] args) {
-		int num=0;
 		Scanner input = new Scanner(System.in);
-
-		while(num != 6) {
+		WorkerManager workerManager=new WorkerManager(input);
+		int num=0;
+		while(num != 5) {
 			System.out.println("1. Add Worker"); 
 			System.out.println("2. Delete Worker"); 
 			System.out.println("3. Edit Worker"); 
 			System.out.println("4. View Worker"); 
-			System.out.println("5. Show a Menu");
-			System.out.println("6. Exit"); 
-			System.out.println("Select one number between 1-6:");
+			System.out.println("5. Exit"); 
+			System.out.println("Select one number between 1-5:");
 			num = input.nextInt();
 			if(num==1) {
-				addWorker();
+				workerManager.addWorker();
 			}
 			else if(num==2) {
-				deleteWorker();
+				workerManager.deleteWorker();
 			}
 			else if(num==3) {
-				editWorker();
+				workerManager.editWorker();
 			}
 			else if(num==4) {
-				viewWorker();
+				workerManager.viewWorker();
 			}
 		}
 
 	}
 	
-	public static void addWorker() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Worker ID:");
-		int workerId = input.nextInt();
-		System.out.print("Worker name:");
-		String workerName = input.next();
-		System.out.print("Email address:");
-		String workerEmail = input.next();
-		System.out.print("Phone number:");
-		String workerPhone = input.next();
-		System.out.print("Worker age:");
-		int workerAge = input.nextInt();
-	}
 	
-	public static void deleteWorker() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Worker ID:");
-		int workerId = input.nextInt();
-	}
-	
-	public static void editWorker() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Worker ID:");
-		int workerId = input.nextInt();
-	}
-	
-	public static void viewWorker() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Worker ID:");
-		int workerId = input.nextInt();
-	}
 }
 
 
